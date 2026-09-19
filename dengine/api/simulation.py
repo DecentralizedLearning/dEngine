20c8bcac6eb42b7f20618e67bbbed62baee9c909
samuelesabella <sabella.samuele.95@gmail.com>
2026-09-19 19:28:14 +0200

    dengine/api/simulation.py: add model download

--- dengine/api/simulation.py
+++ dengine/api/simulation.py
@@ -1,7 +1,14 @@
 from pathlib import Path
 from typing import List, Union
+from urllib.parse import urljoin
+from datetime import datetime, timezone
+import jwt
+from jwt import DecodeError
+import io
 
-from fastapi import APIRouter, Response, status, Depends
+import torch
+from fastapi.responses import StreamingResponse
+from fastapi import APIRouter, Response, status, Depends, HTTPException
 
 from dengine.config.configuration import ExperimentConfiguration
 from dengine.scenarios.event_api.events import Event
@@ -11,7 +18,14 @@ from dengine.scenarios.scenario import GenericClient
 from dengine.bin.simulation import VerbosityLevel
 from dengine.bin.simulation import load_engine, SimulationArguments
 from .dependencies import get_engine
-from .schemas import ClientListSchema
+from .schemas import ClientListSchema, PresignedModelDownloadURL, JWTTokenPresignedModelDownloadURL
+
+
+def decode_token_or_raise_400(token: str, secret: str):
+    try:
+        return jwt.decode(token, secret, algorithms="HS256")  # type: ignore
+    except DecodeError:
+        raise HTTPException(status_code=401, detail="JWT Error")
 
 
 class SimulationAPI:
@@ -19,9 +33,15 @@ class SimulationAPI:
         self,
         output_directory: Path,
         datasets_directory: Path,
+        jwt_secret: str,
+        api_base_url: str,
     ):
         self._output_directory = output_directory
         self._datasets_directory = datasets_directory
+
+        self._jwt_secret = jwt_secret
+        self._presigned_url_get_endpoint = "client/model/download"
+        self._api_base_url = api_base_url
 
         self._router = APIRouter(
             tags=["simulation"],
@@ -32,6 +52,8 @@ class SimulationAPI:
         self._router.add_api_route("/event/all", self.list_all_events, methods=["GET"])
         self._router.add_api_route("/load", self.load, methods=["POST"])
         self._router.add_api_route("/client/all", self.list_all_clients, methods=["GET"])
+        self._router.add_api_route(f"/{self._presigned_url_get_endpoint}", self.client_model_download, methods=["GET"])
+        self._router.add_api_route("/client/{UUID}/model/download", self.client_model_presigned_url, methods=["GET"])
 
     async def sanity_check(self, config: ExperimentConfiguration):
         return Response(status_code=status.HTTP_200_OK)
@@ -82,3 +104,46 @@ class SimulationAPI:
             })
             for c in clients
         ]
+
+    async def client_model_presigned_url(
+        self,
+        UUID: str,
+        engine: DistributedEngine[GenericClient] = Depends(get_engine)
+    ) -> PresignedModelDownloadURL:
+        try:
+            engine.get_client(UUID)
+        except KeyError:
+            raise HTTPException(404, "Unknown client")
+
+        payload = JWTTokenPresignedModelDownloadURL(
+            UUID=UUID,
+            token_created_at=datetime.now(timezone.utc).isoformat(),
+        )
+        token = jwt.encode(payload.model_dump(), self._jwt_secret)
+        return PresignedModelDownloadURL(
+            url=urljoin(self._api_base_url, f"simulation/client/model/download?token={token}")
+        )
+
+    async def client_model_download(
+        self,
+        token: str,
+        engine: DistributedEngine[GenericClient] = Depends(get_engine)
+    ):
+        payload = decode_token_or_raise_400(token, self._jwt_secret)
+        presigned_url_data = JWTTokenPresignedModelDownloadURL.model_validate(payload)
+        try:
+            client = engine.get_client(presigned_url_data.UUID)
+        except KeyError:
+            raise HTTPException(404, "Unknown client")
+
+        buffer = io.BytesIO()
+        torch.save(client.model.state_dict(), buffer)
+        buffer.seek(0)
+
+        return StreamingResponse(
+            content=buffer,
+            media_type="application/octet-stream",
+            headers={
+                "Content-Disposition": f'attachment; filename="{presigned_url_data.UUID}.pt"'
+            }
+        )
